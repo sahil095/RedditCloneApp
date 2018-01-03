4d73139ab9c413695185a85fa680a80809a0741b
sahil095 <sehgal.sahil0786@gmail.com>
2018-01-04 01:37:31 +0530

    Post file added

--- app/src/main/java/com/application/emoji/redditapp/MainActivity.java
+++ app/src/main/java/com/application/emoji/redditapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.application.emoji.redditapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.application.emoji.redditapp.model.Feed;
 import com.application.emoji.redditapp.model.entry.Entry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Callback;
@@ -50,26 +52,45 @@ public class MainActivity extends AppCompatActivity {
 //                Log.d(TAG, "onResponse: updated: " + entries.get(0).getUpdated());
 //                Log.d(TAG, "onResponse: title: " + entries.get(0).getTitle());
 
-                for(int i = 0; i < entries.size();i++){
-                    ExtractXML extractXML1 = new ExtractXML(entries.get(0).getContent(), "<a href=");
+                ArrayList<Post> posts = new ArrayList<Post>();
+
+                for (int i = 0; i < entries.size(); i++) {
+                    ExtractXML extractXML1 = new ExtractXML(entries.get(i).getContent(), "<a href=");
                     List<String> postContent = extractXML1.start();
 
-                    ExtractXML extractXML2 = new ExtractXML(entries.get(0).getContent(), "<img src=");
+                    ExtractXML extractXML2 = new ExtractXML(entries.get(i).getContent(), "<img src=");
 
                     try {
                         postContent.add(extractXML2.start().get(0));
-                    }
-                    catch(NullPointerException e){
+                    } catch (NullPointerException e) {
                         postContent.add(null);
-                        Log.e(TAG, "onResponse: NullPointerException(thumbnails): " + e.getMessage() );
-                    }
-                    catch(IndexOutOfBoundsException e){
+                        Log.e(TAG, "onResponse: NullPointerException(thumbnail):" + e.getMessage());
+                    } catch (IndexOutOfBoundsException e) {
                         postContent.add(null);
-                        Log.e(TAG, "onResponse: IndexOutOfBoundsException(thumbnails): " + e.getMessage());
+                        Log.e(TAG, "onResponse: IndexOutOfBoundsException(thumbnail):" + e.getMessage());
                     }
+
+                    int lastPosition = postContent.size() - 1;
+
+                    posts.add(new Post(
+                            entries.get(i).getTitle(),
+                            entries.get(i).getAuthor().getName(),
+                            entries.get(i).getUpdated(),
+                            postContent.get(0),
+                            postContent.get(lastPosition)
+                    ));
+                }
+
+                for(int j = 0; j < posts.size(); j++)
+                {
+                    Log.d(TAG, "onResponse: \n" +
+                                "PostURL: " + posts.get(j).getPostURL() + "\n" +
+                                "ThumbnailURL: " + posts.get(j).getThumbnailURL() + "\n" +
+                                "Title: " + posts.get(j).getTitle() + "\n" +
+                                "Author: " + posts.get(j).getAuthor() + "\n" +
+                                "Updated: " + posts.get(j).getDate_updated() + "\n" );
                 }
             }
-
             @Override
             public void onFailure(Call<Feed> call, Throwable t) {
                 Log.e(TAG, "onFailure: Unable to Retrieve RSS: " + t.getMessage());
